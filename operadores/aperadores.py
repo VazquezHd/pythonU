053044cd4bd4d43f1c6cfc9acf51aa0c18d3940c
mictlanhalley <97858954+mictlanhalley@users.noreply.github.com>
2024-11-28 20:28:32 -0600

    empezando operadores

---
+++ operadores/aperadores.py
@@ -0,0 +1,92 @@
+"""
+Operadores
+Los operadores en Python son símbolos especiales que se usan para realizar operaciones sobre
+valores y variables. Estos operadores se pueden clasificar en varias categorías según el tipo de
+operación que realizan.
+Tipos de Operadores en Python
+
+• Operadores Aritméticos:
+– Se usan para realizar operaciones matemáticas comunes
+
+• Operadores de Asignación:
+– Se usan para asignar valores a las variables
+
+• Operadores de Comparación:
+– Se usan para comparar dos valores
+
+• Operadores Lógicos:
+– Se usan para comparar dos valores
+
+• Operadores de Identidad:
+– Se usan para comparar los objetos; no si son iguales, sino si son el mismo objeto en
+memoria
+
+• Operadores de Pertenencia:
+– Se usan para probar si una secuencia se presenta en un objeto
+
+
+
+
+
+Operadores aritméticos
+Se usan para realizar las siguientes operaciones matemáticas comunes:
+• Suma: +
+– Ejemplo: 1 + 2 (Resultado = 3)
+• Resta: -
+– Ejemplo: 3 - 2 (Resultado = 1)
+• Multiplicación: *
+– Ejemplo: 2 * 4 (Resultado = 8)
+• División: /
+– Ejemplo: 9 / 2 (Resultado = 4.5)
+• Exponenciación (Calcula la potencia de un número): Se utiliza **
+– Ejemplo: 2 ** 3 (2
+3 = 8, Resultado = 8)
+• División entera (Calcula la parte entera de la división de 2 números): Se utiliza
+//
+– Ejemplo: 10 // 3 (El 3 cabe 3 veces en el 10 y sobra 1, Resultado = 3)
+• Módulo (Calcula el residuo de dividir 2 números): Se utiliza el símbolo %
+– Ejemplo: 10 % 3 (El 3 cabe 3 veces en el 10 y sobra 1, Resultado = 1)
+
+
+
+
+"""
+
+# Suma
+a = 3
+b = 2
+
+suma = a + b
+print(f"Suma: {a} + {b} = {suma}")
+#Resta
+1
+a = 3
+b = 2
+resta = a - b
+print(f"Resta: {a} - {b} = {resta}")
+# Multiplicación
+a = 3
+b = 2
+multiplicacion = a * b
+print(f"Multiplicación: {a} * {b} = {multiplicacion}")
+
+# División
+a = 10
+b = 3
+division = a / b
+print(f"División: {a} / {b} = {division}")
+# Exponenciación
+a = 3
+b = 2
+potencia = a ** b
+print(f"Exponenciación: {a} ** {b} = {potencia}")
+# División entera
+a = 10
+b = 3
+division_entera = a // b
+print(f"División entera: {a} // {b} = {division_entera}")
+# Modulo
+a = 10
+b = 3
+modulo = a % b
+print(f"Modulo: {a} % {b} = {modulo}")
