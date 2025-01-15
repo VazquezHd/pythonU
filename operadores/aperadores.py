21d453ddf7d678cfb95748c3746d9f5199a6917d
mictlanhalley <97858954+mictlanhalley@users.noreply.github.com>
2025-01-14 21:56:42 -0600

    operadores comp

--- operadores/aperadores.py
+++ operadores/aperadores.py
@@ -50,7 +50,6 @@ Se usan para realizar las siguientes operaciones matemáticas comunes:
 
 
 
-"""
 
 # Suma
 a = 3
@@ -88,5 +87,110 @@ print(f"División entera: {a} // {b} = {division_entera}")
 # Modulo
 a = 10
 b = 3
-modulo = a % b
+modulo = a % b     
+# el modulo es el residuo que queda de la division
 print(f"Modulo: {a} % {b} = {modulo}")
+
+
+
+
+#Operadores de asignación   (esta madre lo que hace es hacer la operacion en la misma variable y reasignarlo ahi mismo )
+Se usan para asignar valores a las variables:
+• Asignación : Se utiliza =
+– Ejemplo: a = 3 (Se asigna el valor de 3 a la variable a)
+• Suma y asignación : Se utiliza +=
+– Ejemplo: a += 2 (Se incrementa el valor de a en 2)
+• Resta y asignación : Se utiliza -=
+– Ejemplo: a -= 2 (Decrementa el valor de a en 2)
+• Multiplicación y asignación : Se utiliza *=
+– Ejemplo: a = 2 ( a* se multiplica por 2 y el resultado se guarda de nuevo en a)
+• División y asignación : Se utiliza /=
+– Ejemplo: a /= 2 (a se divide entre 2 y el resultado se guarda de nuevo en a)
+• Exponenciación y asignación : Se utiliza **=
+– Ejemplo: a **= 2 (a se eleva a la segunda potencia y el resultado se guarda de
+nuevo en a)
+• División entera y asignación : Se utiliza //=
+– Ejemplo: a //= 2 (a se divide entre 2 y la parte entera de la división se guarda de
+nuevo en a)
+• Módulo y asignación : Se utiliza %=
+– Ejemplo: a %= 2 (Se calcula el residuo de dividir a entre 2 y este residuo se guarda
+en a)
+
+
+
+
+
+a = 5
+print (f"Asignacion: a = {a}")
+
+#suma y asignacion
+
+a = 5
+a += 3
+print(f"a += 3 = {a}")
+
+#resta y asignacion
+
+a = 5
+a -= 3
+print(f"a -= 3 = {a}")
+
+#multiplicacion y asignacion
+
+a = 5
+a *= 3
+print(f"a *= 3 = {a}")
+
+#division y asignacion
+
+a = 5
+a /= 2
+print(f"a /= 2 = {a}")
+
+
+# esponenciacion y asignacion
+
+a = 5
+a **= 2
+print(f"a **= 2 = {a}")
+
+#division entera y asignacion
+
+a = 5
+a //= 2
+print(f"a //= 2 = {a}")
+
+#modulo y asignacion
+
+a = 5
+a %= 2
+print(f"a %= 2 = {a}")
+
+
+
+
+
+
+Operadores de comparación
+Se usan para comparar dos valores:
+• Igual a : ==
+– Ejemplo: a == b (Compara si a es igual a b. Si son iguales, se obtiene un True. Si
+no son iguales, se obtiene un False)
+• No igual a : !=
+– Ejemplo: a != b (Compara si a es diferente de b. Si son diferentes, se obtiene un
+True. Si son iguales, se obtiene un False)
+• Mayor que : >
+– Ejemplo: a > b (Compara si a es mayor a b. Si a es mayor, se obtiene un True. Si a
+es menor, se obtiene un False)
+• Menor que : <
+– Ejemplo: a < b (Compara si a es menor a b. Si a es menor, se obtiene un True. Si
+a es mayor, se obtiene un False)
+• Mayor o igual que : Se utiliza >=
+– Ejemplo: a >= b (Compara si a es mayor o igual a b. Si a es mayor o igual a b, se
+obtiene un True. Si a es menor a b, se obtiene un False)
+• Menor o igual que : Se utiliza <=
+– Ejemplo: a <= b (Compara si a es menor o igual a b. Si a es menor o igual a b, se
+obtiene un True. Si a es mayor a b, se obtiene un False)
+
+"""
+
